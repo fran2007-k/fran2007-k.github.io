02e6bd0a28541bbb70a8a3d6de466e782f896d4e
Francisco F.B <ffernandezbeccari@gmail.com>
2023-08-24 20:14:35 +0200

    moved index.html to src

--- src/App.jsx
+++ src/App.jsx
@@ -52,7 +52,7 @@ function App() {
 
       <footer className='p-12 h-fit bg-gray-500 flex justify-between'>
         <span className='text-gray-300 font-extralight'>The Kickboxing Hub</span>
-        {/* Remember to change the mail */}
+        {/* Remember to change the mail and change the icon*/}
         <a href='mailto:ffernandezbeccari@hotmail.com' className='text-gray-300 font-extralight border-b border-solid border-gray-300'>Mail</a>
       </footer>
     </>
