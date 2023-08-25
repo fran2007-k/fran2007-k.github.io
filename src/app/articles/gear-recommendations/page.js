3665bdb3de2488f9dbdf91533bd1270b35403095
Francisco F.B <ffernandezbeccari@gmail.com>
2023-08-25 19:47:43 +0200

    Changed to nextjs

---
+++ src/app/articles/gear-recommendations/page.js
@@ -0,0 +1,12 @@
+import Nav from '../../page.js'
+
+function Gear() {
+    return (
+        <>
+            {Nav()}
+            <h1>Gear Recommendations</h1>
+        </>
+    )
+}
+
+export default Gear
